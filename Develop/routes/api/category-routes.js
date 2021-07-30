e061bf2f8ef68c7466cdfce14ee2df25ca8cb0b8
Landon Waddell <Landon.waddell@gmail.com>
2021-07-30 12:00:50 -0600

    sync sequelize models to the database and added properties to models. Started on api routes request.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,37 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  }).then(findRes => res.json(findRes))
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({ 
+    where: { id: req.params.id },
+    include: [Product]
+  }).then(findRes => res.json(findRes))
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then(newCategory => res.json(newCategory))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({ 
+    where: { id: req.params.id },
+    include: [Product]
+  }).then(findRes => res.json(findRes))
 });
 
 module.exports = router;
